a3e07944fec3b7744a0e9cb056c398d8b0b55e48
Eric Kim <bosung90@gmail.com>
2021-07-04 10:35:18 -0700

    Revert "Add git hook, prettier, eslint and JSS"
    This reverts commit 9c3b78b242347dbe19c4950438c17ac21b974057.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React, { memo } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <div>Welcome, KDD steppers!</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default memo(App);
+export default App;
