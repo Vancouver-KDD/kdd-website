f022d28e2368fcea8cc6db9edd2188ddadda4354
Eric Kim <bosung90@gmail.com>
2021-07-04 10:55:20 -0700

    Revert "Revert "Add git hook, prettier, eslint and JSS""
    This reverts commit a3e07944fec3b7744a0e9cb056c398d8b0b55e48.

---
+++ src/components/DemoJss.js
@@ -0,0 +1,30 @@
+import React, { memo } from 'react';
+import { createUseStyles } from 'react-jss';
+
+// Simple Demo component that shows the usage of react-jss in a very simple way
+function DemoJss() {
+  const classes = useStyles();
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.circle} />
+    </div>
+  );
+}
+
+const useStyles = createUseStyles({
+  wrapper: {
+    backgroundColor: 'grey',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '400px',
+  },
+  circle: {
+    width: '50px',
+    height: '50px',
+    borderRadius: '50%',
+    backgroundColor: '#232323',
+  },
+});
+
+export default memo(DemoJss);
