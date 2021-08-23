afdb6451a9b6773050907d149836774231ad530c
hj lee <imhyojunglee@gmail.com>
2021-08-23 00:16:32 -0700

    fix photo grouping & add moment lib & fix footer style

--- src/pages/Photospage.js
+++ src/pages/Photospage.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react'
 import {createUseStyles} from 'react-jss'
+import moment from 'moment';
 import {Button} from 'common/Button'
 import NavigationBar from 'components/NavigationBar'
 import PhotoCard from 'components/PhotoCard'
 import Footer from 'components/Footer'
 import {Modal} from 'common/Modal'
 import {useCollection} from 'store'
+
 
 const Photospage = () => {
     const classes = useStyles()
@@ -45,18 +47,21 @@ const Photospage = () => {
                 <h1>Photos</h1>
                 <div className={classes.photoBox}>
                     <div className={classes.photoCardList}>
-                        {/* <h2>June 14, 2021</h2> */}
-                        {photos?.map((photo) => {
+                        {photos?.map((photo, index) => {
                             const imageUrl = photo?.photo?.[0]?.formats?.medium?.url || photo?.photo?.[0]?.url
                             return (
-                                <PhotoCard
-                                    key={photo.id}
-                                    src={imageUrl}
-                                    alt={photo.description}
-                                    description={photo.description}
-                                    author={photo.uploader_name}
-                                    onClick={() => handlPhroCardClick(imageUrl, photo.description, photo.uploader_name)}
-                                />
+                                <>
+                                    {!!index||<h2>{moment(photo.created_at).format('LL')}</h2>}
+                                    {moment(photo.created_at).format('LL') === moment(photos[Math.max(index - 1, 0)].created_at).format('LL')||<h2>{moment(photo.created_at).format('LL')}</h2>}
+                                    <PhotoCard
+                                        key={photo.id}
+                                        src={imageUrl}
+                                        alt={photo.description}
+                                        description={photo.description}
+                                        author={photo.uploader_name}
+                                        onClick={() => handlPhroCardClick(imageUrl, photo.description, photo.uploader_name)}
+                                    />
+                                </>
                             )
                         })}
                     </div>
