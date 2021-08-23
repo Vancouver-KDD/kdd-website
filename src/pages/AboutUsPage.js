9dbbcef585383b85bed3501759291dcdc2aad307
jka236 <jka236@sfu.ca>
2021-08-23 14:39:25 -0700

    second about-us section added

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -5,6 +5,9 @@ import Footer from 'components/Footer'
 import aboutUsNetworking from 'assets/icons/aboutUsNetworking.svg'
 import aboutUsCareer from 'assets/icons/aboutUsCareer.svg'
 import aboutUsSoftLanding from 'assets/icons/aboutUsSoftLanding.svg'
+import {Button} from 'common/Button'
+import slackLogo from 'assets/icons/coolicon-slack.svg'
+import {Image} from 'common/Image'
 
 export default function AboutUsPage() {
     const classes = useStyles()
@@ -12,7 +15,7 @@ export default function AboutUsPage() {
     return (
         <div>
             <NavigationBar />
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', marginBottom: 100}}>
                 <h1 style={{marginBottom: 40}}><h1 className={classes.missionTitle}>KDD's</h1> Mission</h1>
                 <div style={{display: 'flex', width: 1024, maxWidth: '100%', justifyContent: 'space-between'}}>
                     <Mission
@@ -32,6 +35,26 @@ export default function AboutUsPage() {
                     />
                 </div>
             </div>
+
+            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center',backgroundColor:'#EAEAEA', height: 350}}>
+                <h1 style={{marginBottom: 40, marginTop: 40}}>It all started in July 2017</h1>
+                <div style={{display: 'flex', width: 900, maxWidth: '100%', justifyContent: 'space-between'}}>
+                    <h5 className={classes.missionDescription}>
+                    KDD is for developers and designers who are interested in networking and sharing ideas with like-minded people. 
+                    {'\n'}We organize events that provide opportunities to make friends, learn new things, and improve your skills. 
+                    {'\n'}Events will include technical talks, 
+                    socials, and workshops.
+                    </h5>
+                </div>
+                <div className={classes.btnContainer}>
+                    <Button
+                        href="https://join.slack.com/t/vancouverkdd/shared_invite/zt-smt20gzk-OBnpJFijGnVKaT80RdgD9g"
+                        newTab
+                        Icon={<Image img={slackLogo} alt="Join Slack" />}
+                        text="Join Slack"
+                    />
+                </div>
+            </div>
             <Footer />
         </div>
     )
@@ -45,12 +68,20 @@ const useStyles = createUseStyles((theme) => ({
     missionDescription: {
         marginTop: 5,
         whiteSpace: 'pre-wrap',
-        fontSize: '2rem'
-
+        fontSize: '1.5rem',
+        textAlign: 'center',
     },
     title: {
         color: theme.colorPrimary,
-    }
+    },
+    btnContainer: {
+        marginTop: 40,
+        marginLeft: 5,
+        marginRight: 5,
+        width: 200,
+        display: 'flex',
+        flexDirection: 'column',
+    },
 }))
 
 const Mission = ({Image, title, description}) => {
