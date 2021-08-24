f4ff416316997c1b1f9ecda58f520d3a46d75f98
Eric Kim <bosung90@gmail.com>
2021-08-24 16:50:46 -0700

    update photos api to fetch with created_at:DESC

--- src/api/index.js
+++ src/api/index.js
@@ -10,11 +10,11 @@ export async function getSponsors() {
 /**
  * Query photos
  * @param  {Number} [offset=0] offset to fetch photos from
- * @param  {Number} [limit=12] number of photos to fetch
+ * @param  {Number} [limit=6] number of photos to fetch
  * @returns {Promise} a promise that resolves to array of photos
  */
-export async function getPhotos({offset = 0, limit = 12} = {offset: 0, limit: 12}) {
-    const res = await fetch(`https://kdd-server.herokuapp.com/photos?_start=${offset}&_limit=${limit}`)
+export async function getPhotos({offset = 0, limit = 6} = {offset: 0, limit: 6}) {
+    const res = await fetch(`https://kdd-server.herokuapp.com/photos?_sort=created_at:DESC&_start=${offset}&_limit=${limit}`)
     const data = await res.json()
     return data
 }
