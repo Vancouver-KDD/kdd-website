d68fb8a939354b8565c8aa3eb62a4c5e19b5b527
jka236 <jka236@sfu.ca>
2021-09-02 16:08:40 -0700

    event card styling WIP

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -8,6 +8,7 @@ import tempEventImg from 'assets/images/temp-event.png'
 import moment from 'moment'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import {Space} from 'components' 
 
 function EventCard({title, date, description, location}) {
     const classes = useStyles()
@@ -23,13 +24,18 @@ function EventCard({title, date, description, location}) {
             <img className={classes.eventImage} src={`${tempEventImg}`} alt="Temp Event Card" />
             <div className={classes.eventInfoContainer}>
                 <div className={classes.eventInfo}>
-                    <p className={classes.eventDate}>{dateStr}</p>
+                    <p className={classes.eventDate}>
+                        {dateStr} | {location}
+                    </p>
                     <h2>{title}</h2>
-                    <ReactMarkdown remarkPlugins={[remarkGfm]}>{description}</ReactMarkdown>
-                    <p className={classes.eventLocation}>{location}</p>
+                    {/* <ReactMarkdown remarkPlugins={[remarkGfm]}>{description}</ReactMarkdown> */}
+                    <p className={classes.eventDescription}>{description}</p>
                 </div>
                 <div className={classes.eventBtnGroup}>
-                    <SignupButton />
+                    <div style={{flex:1}}>
+                        <SignupButton />
+                    </div>
+                    <Space y1={10} />
                     {isCalTooltipDisplay && <CalendarTooltip />}
                     <CalendarButton onClick={handleClick} />
                 </div>
@@ -50,23 +56,6 @@ const useStyles = createUseStyles(() => ({
         display: 'flex',
         flexDirection: 'row',
         overflow: 'hidden',
-        // '& img': {
-        // width: '373px',
-        // objectFit: 'cover',
-        // borderStartStartRadius: '16px',
-        // borderStartEndRadius: '16px',
-        // },
-        // '& h2': {
-        //     fontSize: '2.2rem',
-        // },
-        // '@media (min-width: 1024px)': {
-        // width: '502px',
-        // height: '442px',
-        // margin: '2px 5px',
-        // '& img': {
-        //     width: '502px',
-        // },
-        // },
     },
     eventImage: {
         width: '50%',
@@ -74,32 +63,37 @@ const useStyles = createUseStyles(() => ({
         aspectRatio: '16 / 10',
     },
     eventInfoContainer: {
-        backgroundColor: '#eee',
+        backgroundColor: '#F5F5F5',
         width: '50%',
         display: 'flex',
         flexDirection: 'column',
+        padding: 25,
+        flex: 1,
     },
     eventInfo: {
         display: 'flex',
         height: '12rem',
-        padding: '10px 20px',
         flexDirection: 'column',
-        justifyContent: 'space-evenly',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
     },
     eventDate: {
-        fontSize: '1.5rem',
-        lineHeight: '19px',
+        fontSize: '1.8rem',
     },
-    eventLocation: {
-        fontSize: '1.5rem',
-        lineHeight: '19px',
+    eventTitle: {
+        fontSize: '2.4rem',
+    },
+    eventDescription: {
+        fontSize: '1.8rem',
+        flex: 1,
+        display: 'flex',
     },
     eventBtnGroup: {
         display: 'flex',
-        justifyContent: 'space-evenly',
-        height: '12rem',
-        padding: '10px 20px',
-        position: 'relative',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 }))
 
