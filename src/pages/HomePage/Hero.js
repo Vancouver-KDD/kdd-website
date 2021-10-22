ec45f9f70278f3efccf628252fae31217ce3ad8f
xuyunzeng@outlook.com <xuyunzeng@outlook.com>
2021-10-22 12:39:35 -0700

    hero and stat divs and text updated

--- src/pages/HomePage/Hero.js
+++ src/pages/HomePage/Hero.js
@@ -1,5 +1,3 @@
-import React from 'react'
-import classnames from 'classnames'
 import {Image} from 'components/Image'
 import {Button} from 'components/buttons/Button'
 import {createUseStyles} from 'react-jss'
@@ -7,15 +5,29 @@ import slackLogo from 'assets/icons/coolicon-slack.svg'
 import linkedInIcon from 'assets/icons/LinkedIn.svg'
 import heroBG from 'assets/images/hero-bg.jpg'
 import ellipse from 'assets/images/ellipse.svg'
-import {Typography} from '@mui/material'
+import {Typography, Box} from '@mui/material'
 
 export default function Hero() {
     const classes = useStyles()
 
     return (
-        <div className={classes.container}>
-            <div className={classnames('kdd-hero', classes.default)}>
-                <div className={classes.contentContainer}>
+        <Box height="100vh" minHeight={68} position="relative">
+            <Box
+                sx={{
+                    background: `url(${heroBG}) no-repeat center center`,
+                    backgroundSize: 'cover',
+                    height: '100%',
+                }}>
+                <Box
+                    sx={{
+                        textAlign: 'center',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        padding: 2,
+                        paddingTop: 14,
+                        justifyContent: 'center',
+                        height: '100%',
+                    }}>
                     <Typography
                         variant="h3"
                         sx={{
@@ -37,74 +49,49 @@ export default function Hero() {
                         저희는 밴쿠버 한인 개발자 디자이너로 이루어져 있으며 네트워킹 및 한인 사회에 기여를 추구하는 모임 입니다.
                     </Typography>
 
-                    <div style={{display: 'flex', justifyContent: 'center'}}>
-                        <div className={classes.btnContainer}>
+                    <Box justifyContent="center" flexDirection="row">
+                        <Box
+                            sx={{
+                                marginLeft: 1,
+                                marginRight: 1,
+
+                                display: 'flex',
+                                flexDirection: 'column',
+                            }}>
                             <Button
                                 href="https://www.linkedin.com/company/vancouver-kdd"
                                 newTab
                                 Icon={<Image img={linkedInIcon} alt="LinkedIn" />}
                                 text="LinkedIn"
                             />
-                        </div>
-                        <div className={classes.btnContainer}>
+                        </Box>
+                        <Box
+                            sx={{
+                                marginLeft: 1,
+                                marginRight: 1,
+
+                                display: 'flex',
+                                flexDirection: 'column',
+                            }}>
                             <Button
                                 href="https://join.slack.com/t/vancouverkdd/shared_invite/zt-smt20gzk-OBnpJFijGnVKaT80RdgD9g"
                                 newTab
                                 Icon={<Image img={slackLogo} alt="Join Slack" />}
                                 text="Join Slack"
                             />
-                        </div>
-                    </div>
-                </div>
-            </div>
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
             <img className={classes.divider} src={ellipse} alt="ellipse divider" />
-        </div>
+        </Box>
     )
 }
 
 const useStyles = createUseStyles({
-    container: {
-        height: '100vh',
-        minHeight: 550,
-        position: 'relative',
-    },
     divider: {
         width: '100%',
         position: 'absolute',
         bottom: -1,
-    },
-    default: {
-        background: `url(${heroBG}) no-repeat center center`,
-        backgroundSize: 'cover',
-        height: '100%',
-    },
-    contentContainer: {
-        textAlign: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        padding: 20,
-        paddingTop: 110,
-        justifyContent: 'center',
-        height: '100%',
-    },
-    title: {
-        textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-        color: 'white',
-        fontWeight: '900',
-        // fontSize: '3.6rem',
-        marginBottom: '3rem',
-    },
-    subtitle: {
-        color: 'white',
-        // fontSize: '1.8rem',
-        fontWeight: '700',
-        marginBottom: '3rem',
-    },
-    btnContainer: {
-        marginLeft: 5,
-        marginRight: 5,
-        width: 130,
-        display: 'flex',
-        flexDirection: 'column',
     },
 })
--- src/pages/HomePage/Stats.js
+++ src/pages/HomePage/Stats.js
@@ -1,79 +1,75 @@
-import React from 'react'
-import {createUseStyles} from 'react-jss'
 import StatSmall from 'assets/images/stat-small.png'
 import StatLarge from 'assets/images/stat-large.png'
 import {useCollection} from 'store'
+import {Typography, Box} from '@mui/material'
 
 export default function Stats() {
-    const classes = useStyles()
     const {data} = useCollection({name: 'statistics'})
 
     return (
-        <section className={classes.container}>
-            <div className={classes.containerContent}>
-                <div className={classes.image} alt="Statistic image" />
-                <div className={classes.statInfo}>
-                    <h1>Since July 2017</h1>
-                    <div>{data?.meetups ?? '39+'} Meetups</div>
-                    <div>{data?.members ?? '1250+'} Members</div>
-                </div>
-            </div>
-        </section>
+        <Box
+            component="section"
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                backgroundColor: 'hsla(0, 0%, 100%, 1)',
+                textAlign: 'center',
+            }}>
+            <Box
+                sx={{
+                    height: 543,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    width: 1024,
+                    maxWidth: '100%',
+                    '@media (min-width: 768px)': {
+                        alignItems: 'initial',
+                        display: 'grid',
+                        gridTemplateColumns: '1fr 1fr',
+                    },
+                }}>
+                <Box
+                    sx={{
+                        backgroundImage: `url(${StatSmall})`,
+                        backgroundColor: 'white',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center center',
+                        height: 250,
+                        width: 250,
+                        '@media (min-width: 768px)': {
+                            width: 'unset',
+                            height: 'inherit',
+                        },
+                        '@media (min-width: 1024px)': {
+                            backgroundImage: `url(${StatLarge})`,
+                        },
+                    }}
+                    alt="Statistic image"
+                />
+                <Box
+                    mt={5}
+                    mb={5}
+                    sx={{
+                        color: 'rgba(14, 14, 44, 1)',
+                        '& > h3': {
+                            marginTop: 3,
+                            marginBottom: 3,
+                            fontWeight: 700,
+                        },
+                        '@media (min-width: 768px)': {
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'center',
+                        },
+                    }}>
+                    <Typography variant="h3">Since July 2017</Typography>
+                    <Typography variant="h3">{data?.meetups ?? '39+'} Meetups</Typography>
+                    <Typography variant="h3">{data?.members ?? '1250+'} Members</Typography>
+                </Box>
+            </Box>
+        </Box>
     )
 }
-
-const useStyles = createUseStyles({
-    container: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        backgroundColor: 'hsla(0, 0%, 100%, 1)',
-        textAlign: 'center',
-    },
-    containerContent: {
-        height: 543,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: 1024,
-        maxWidth: '100%',
-        '@media (min-width: 768px)': {
-            alignItems: 'initial',
-            display: 'grid',
-            gridTemplateColumns: '1fr 1fr',
-        },
-    },
-    image: {
-        backgroundImage: `url(${StatSmall})`,
-        backgroundColor: 'white',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center center',
-        height: 250,
-        width: 250,
-        '@media (min-width: 768px)': {
-            width: 'unset',
-            height: 'inherit',
-        },
-        '@media (min-width: 1024px)': {
-            backgroundImage: `url(${StatLarge})`,
-        },
-    },
-    statInfo: {
-        // fontSize: '3.6rem',
-        fontWeight: '900',
-        marginTop: '2.5rem',
-        marginBottom: '2.5rem',
-        color: 'rgba(14, 14, 44, 1)',
-        '& > div': {
-            marginTop: '2.5rem',
-            marginBottom: '2.5rem',
-        },
-        '@media (min-width: 768px)': {
-            // fontSize: '4.8rem',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-        },
-    },
-})
