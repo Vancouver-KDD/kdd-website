01f64caab0dccc00365d5ae0f2fe50029073f462
Sarah Son <plmn855000@gmail.com>
2021-10-23 15:31:08 -0700

    change build failed problem

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
-import {useRouteMatch} from 'react-router-dom'
-import {createUseStyles} from 'react-jss'
 import NavigationBar from 'components/NavigationBar'
 import Footer from 'components/Footer'
-import {Space} from 'components'
 
 export default function BlogPage() {
     return (
