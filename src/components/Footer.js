d4df05468b97ca6c0ecb985c2ada1459bb5f9f13
xuyunzeng@outlook.com <xuyunzeng@outlook.com>
2021-10-29 19:45:10 -0700

    updated footer and photopage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,68 +1,52 @@
 import React from 'react'
-import {createUseStyles} from 'react-jss'
 import {useCollection} from 'store'
+import {Typography, Stack, Box, Link} from '@mui/material'
 
 export default function Footer() {
-    const classes = useStyles()
     const {data} = useCollection({name: 'statistics'})
 
     return (
-        <footer className={classes.footer}>
-            <p>
+        <Box
+            component="footer"
+            sx={{
+                height: 'auto',
+                padding: 1,
+                color: 'hsla(240, 52%, 11%, 1)',
+                fontWeight: '700',
+                textAlign: 'center',
+                padding: {
+                    xs: 0.5,
+                    sm: 3,
+                    md: 6,
+                },
+
+                '& div': {
+                    mt: 1,
+                },
+                '& a': {
+                    padding: {
+                        xs: 0.5,
+                        sm: 0.75,
+                    },
+                    color: 'hsla(240, 52%, 11%, 1)',
+                },
+            }}>
+            <Box>
                 Developed by{' '}
                 {data?.developers?.map?.(({name, link}, i) => (
-                    <a key={i} href={link}>
+                    <Link key={i} href={link}>
                         {name}
-                    </a>
+                    </Link>
                 ))}
-            </p>
-            <p>
+            </Box>
+            <Box>
                 Special thanks to{' '}
                 {data?.supporters?.map?.(({name, link}, i) => (
-                    <a key={i} href={link}>
+                    <Link key={i} href={link}>
                         {name}
-                    </a>
+                    </Link>
                 ))}
-            </p>
-        </footer>
+            </Box>
+        </Box>
     )
 }
-
-const useStyles = createUseStyles({
-    footer: {
-        height: 'auto',
-        padding: '1rem',
-        color: 'hsla(240, 52%, 11%, 1)',
-        fontWeight: '900',
-        textAlign: 'center',
-        '& p': {
-            // fontSize: '1.2rem',
-            marginTop: 10,
-        },
-        '& a': {
-            padding: '0.5rem',
-            // fontSize: '1.2rem',
-            color: 'hsla(240, 52%, 11%, 1)',
-        },
-        '@media (min-width: 800px)': {
-            padding: '3rem',
-            '& p': {
-                // fontSize: '1.6rem',
-            },
-            '& a': {
-                padding: '0.75rem',
-                // fontSize: '1.6rem',
-            },
-        },
-        '@media (min-width: 1024px)': {
-            padding: '6rem',
-            '& p': {
-                // fontSize: '2rem',
-            },
-            '& a': {
-                padding: '0.75rem',
-                // fontSize: '2rem',
-            },
-        },
-    },
-})
--- src/pages/PhotosPage.js
+++ src/pages/PhotosPage.js
@@ -1,13 +1,11 @@
 import React, {useState} from 'react'
-import {createUseStyles} from 'react-jss'
 import moment from 'moment'
 import {Button} from 'components/buttons'
 import {Footer, Modal, NavigationBar, PhotoCard} from 'components'
 import {useCollection} from 'store'
+import {Typography, Stack, Box} from '@mui/material'
 
 export default function PhotosPage() {
-    const classes = useStyles()
-
     const [isModalVisible, setIsModalVisible] = useState(false)
     const [photoUrl, setPhotoUrl] = useState('')
     const [photoDescription, setPhotoDescription] = useState('')
@@ -30,7 +28,7 @@ export default function PhotosPage() {
     }
 
     return (
-        <div>
+        <>
             <NavigationBar />
             <Modal
                 photoUrl={photoUrl}
@@ -39,15 +37,33 @@ export default function PhotosPage() {
                 visible={isModalVisible}
                 toggleModal={toggleModal}
             />
-            <div className={classes.wrap}>
-                <h1>Photos</h1>
-                <div className={classes.photoBox}>
-                    <div className={classes.photoCardList}>
+            <Box
+                width={{
+                    xs: 375,
+                    sm: 550,
+                    md: 1024,
+                }}
+                position="relative"
+                margin="0 auto">
+                <Typography mb={3} variant="h4" fontWeight="700">
+                    Photos
+                </Typography>
+                <Stack>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            flexWrap: 'wrap',
+                        }}>
                         {photos?.map?.((photo, index) => {
                             const imageUrl = photo.photo?.[0]?.formats?.medium?.url || photo.photo?.[0]?.url
                             return (
                                 <React.Fragment key={photo.id}>
-                                    {isDisplayCreateDate(index, photos) && <h2>{moment(photo.created_at).format('LL')}</h2>}
+                                    {isDisplayCreateDate(index, photos) && (
+                                        <Typography my={0.5} width="100%" fontWeight="700" variant="h4">
+                                            {moment(photo.created_at).format('LL')}
+                                        </Typography>
+                                    )}
                                     <PhotoCard
                                         src={imageUrl}
                                         alt={photo.description}
@@ -58,16 +74,16 @@ export default function PhotosPage() {
                                 </React.Fragment>
                             )
                         })}
-                    </div>
-                </div>
-            </div>
-            <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                    </Box>
+                </Stack>
+            </Box>
+            <Stack alignItems="center">
                 {loading && <span>loading...</span>}
                 {!!error && <span>ERROR: {error.message}</span>}
                 <Button onClick={loadMore} text={'Load More Photos'} />
-            </div>
+            </Stack>
             <Footer />
-        </div>
+        </>
     )
 }
 
@@ -79,40 +95,3 @@ const isDisplayCreateDate = (index = 0, photos = null) => {
 
     return !moment(currentCreateDate).isSame(previousCreateDate, 'day')
 }
-
-const useStyles = createUseStyles({
-    wrap: {
-        position: 'relative',
-        width: 375,
-        margin: 'auto',
-        '@media (min-width: 800px)': {
-            width: 758,
-        },
-        '@media (min-width: 1024px)': {
-            width: 1024,
-        },
-    },
-    photoBox: {
-        position: 'relative',
-        margin: '3rem 0px',
-    },
-    photoCardList: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& h2': {
-            fontSize: '2.8rem',
-            fontWeight: '600',
-            margin: '4px 0px',
-            width: 375,
-            '@media (min-width: 800px)': {
-                width: 758,
-            },
-            '@media (min-width: 1024px)': {
-                width: 1024,
-            },
-        },
-    },
-    photoCardPick: {
-        color: 'black',
-    },
-})
