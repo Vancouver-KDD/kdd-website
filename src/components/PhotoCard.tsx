268848af314af877c109f52fe74dac4aafffce81
tuxinblue <tombow12@gmail.com>
2021-12-03 13:29:19 -0800

    전체 사진 보이도록 수정

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,44 +1,52 @@
 import React, {useState} from 'react'
 import {Dialog, DialogTitle, DialogContent, DialogContentText, Stack, Box, ButtonBase, Typography} from '@mui/material'
 import type {PhotoType} from 'types'
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 export default function PhotoCard({photo, title, description}: PhotoType) {
-    const imageUrl = photo?.[0]?.formats?.medium?.url || photo?.[0]?.url
+    //const imageUrl = photo?.[0]?.formats?.medium?.url || photo?.[0]?.url
+    const [imageUrl, setImageUrl] = useState(photo?.[0]?.formats?.medium?.url || photo?.[0]?.url)
     const [isOpen, setIsOpen] = useState(false)
+    const handleClickOpen = (imageUrl: string) => () => {
+        setIsOpen(true);
+        setImageUrl(imageUrl);
+      };
 
     return (
         <>
             <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+                
                 <DialogTitle sx={{fontWeight: 'bold', textAlign: 'center'}}>{title}</DialogTitle>
                 <DialogContent>
                     <Box component="img" sx={{width: '100%'}} src={imageUrl} alt={description} />
                     <DialogContentText>{description}</DialogContentText>
                 </DialogContent>
             </Dialog>
+
             <Stack alignItems="center">
-                <ButtonBase disableRipple onClick={() => setIsOpen(true)}>
+                
                     <Stack>
-                        <Box
-                            component="img"
-                            sx={{
-                                borderRadius: 2,
-                                maxHeight: 250,
-                                aspectRatio: '16/9',
-                                maxWidth: 'sm',
-                                width: '100%',
-                                height: '100%',
-                                objectFit: 'cover',
-                            }}
-                            src={imageUrl}
-                            loading="lazy"
-                            alt={description}
-                        />
+                        <ImageList sx={{ width: '100%', height: '100%', maxHeight: 500 }} cols={2} rowHeight={164}>
+                            {photo.map((item) => (
+                            <ButtonBase disableRipple onClick={handleClickOpen(item.url)}>
+                            <ImageListItem key={item.name}>
+                            <img
+                                src={`${item.url}?w=164&h=164&fit=crop&auto=format`}
+                                srcSet={`${item.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                                alt={item.name}
+                                loading="lazy"
+                            />
+                            </ImageListItem>
+                            </ButtonBase>
+                            ))}
+                        </ImageList>
                         <Typography variant="h6" fontWeight="bold" textAlign="center">
                             {title}
                         </Typography>
                         <Typography variant="body1">{description}</Typography>
                     </Stack>
-                </ButtonBase>
+                
             </Stack>
         </>
     )
