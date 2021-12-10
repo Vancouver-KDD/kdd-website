f6d51cdd5b8b3f078b0c01f7023f61d9ad09b94a
tuxinblue <tombow12@gmail.com>
2021-12-10 15:57:02 -0800

    Applied Carousel UI component (react-material-ui-carousel) on Photo pages

--- src/components/PhotoCard.tsx
+++ src/components/PhotoCard.tsx
@@ -1,52 +1,52 @@
 import React, {useState} from 'react'
-import {Dialog, DialogTitle, DialogContent, DialogContentText, Stack, Box, ButtonBase, Typography} from '@mui/material'
+import {Dialog, DialogTitle, DialogContent, DialogContentText, Stack, Box, Typography, Paper} from '@mui/material'
 import type {PhotoType} from 'types'
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import Carousel from 'react-material-ui-carousel'
 
 export default function PhotoCard({photo, title, description}: PhotoType) {
-    //const imageUrl = photo?.[0]?.formats?.medium?.url || photo?.[0]?.url
     const [imageUrl, setImageUrl] = useState(photo?.[0]?.formats?.medium?.url || photo?.[0]?.url)
     const [isOpen, setIsOpen] = useState(false)
     const handleClickOpen = (imageUrl: string) => () => {
-        setIsOpen(true);
-        setImageUrl(imageUrl);
-      };
+        setIsOpen(true)
+        setImageUrl(imageUrl)
+    }
 
     return (
         <>
             <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-                
                 <DialogTitle sx={{fontWeight: 'bold', textAlign: 'center'}}>{title}</DialogTitle>
                 <DialogContent>
                     <Box component="img" sx={{width: '100%'}} src={imageUrl} alt={description} />
                     <DialogContentText>{description}</DialogContentText>
                 </DialogContent>
             </Dialog>
-
-            <Stack alignItems="center">
-                
-                    <Stack>
-                        <ImageList sx={{ width: '100%', height: '100%', maxHeight: 500 }} cols={2} rowHeight={164}>
-                            {photo.map((item) => (
-                            <ButtonBase disableRipple onClick={handleClickOpen(item.url)}>
-                            <ImageListItem key={item.name}>
+            <Stack>
+                <Typography variant="h6" fontWeight="bold" textAlign="left">
+                    {title}
+                </Typography>
+                <Typography variant="body1">{description}</Typography>
+                <Carousel
+                    fullHeightHover={false}
+                    navButtonsProps={{
+                        style: {
+                            backgroundColor: 'tan',
+                            borderRadius: 0,
+                        },
+                    }}>
+                    {photo.map((item, i) => (
+                        <Paper>
                             <img
-                                src={`${item.url}?w=164&h=164&fit=crop&auto=format`}
-                                srcSet={`${item.url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                                alt={item.name}
-                                loading="lazy"
+                                key={i}
+                                src={item.url}
+                                alt={title}
+                                height="100%"
+                                width="100%"
+                                onClick={handleClickOpen(item.url)}
+                                style={{cursor: 'pointer'}}
                             />
-                            </ImageListItem>
-                            </ButtonBase>
-                            ))}
-                        </ImageList>
-                        <Typography variant="h6" fontWeight="bold" textAlign="center">
-                            {title}
-                        </Typography>
-                        <Typography variant="body1">{description}</Typography>
-                    </Stack>
-                
+                        </Paper>
+                    ))}
+                </Carousel>
             </Stack>
         </>
     )
